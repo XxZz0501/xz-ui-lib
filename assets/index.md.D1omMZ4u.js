8a0a65aad2ce77e4c53f1901f3551401e5d800dd
张家豪 <1750181817@163.com>
2025-12-31 08:35:27 +0800

    Updates

--- assets/index.md.D1omMZ4u.js
+++ assets/index.md.D1omMZ4u.js
@@ -1,6 +1,4 @@
-import{_ as n,c as a,o as p,ae as e}from"./chunks/framework.D69qwIbp.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),t={name:"index.md"};function l(i,s,c,o,d,r){return p(),a("div",null,[...s[0]||(s[0]=[e(`<div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>---</span></span>
-<span class="line"><span># https://vitepress.dev/reference/default-theme-home-page</span></span>
-<span class="line"><span>layout: home</span></span>
+import{_ as n,c as a,o as p,ae as e}from"./chunks/framework.D69qwIbp.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),t={name:"index.md"};function l(i,s,c,o,d,r){return p(),a("div",null,[...s[0]||(s[0]=[e(`<div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>layout: home</span></span>
 <span class="line"><span></span></span>
 <span class="line"><span>hero:</span></span>
 <span class="line"><span>  name: &quot;XZ UI&quot;</span></span>
@@ -12,7 +10,7 @@ import{_ as n,c as a,o as p,ae as e}from"./chunks/framework.D69qwIbp.js";const _
 <span class="line"><span>      link: /guide/introduction</span></span>
 <span class="line"><span>    - theme: alt</span></span>
 <span class="line"><span>      text: GitHub</span></span>
-<span class="line"><span>      link: https://github.com/your-name/xz-ui-lib</span></span>
+<span class="line"><span>      link: https://github.com/XxZz0501/xz-ui-lib</span></span>
 <span class="line"><span></span></span>
 <span class="line"><span>features:</span></span>
 <span class="line"><span>  - title: 🧰 工具函数</span></span>
@@ -20,5 +18,4 @@ import{_ as n,c as a,o as p,ae as e}from"./chunks/framework.D69qwIbp.js";const _
 <span class="line"><span>  - title: ⚡ 组合式函数</span></span>
 <span class="line"><span>    details: useFetch、useStorage 等响应式逻辑封装</span></span>
 <span class="line"><span>  - title: 🎨 高质量组件</span></span>
-<span class="line"><span>    details: 按钮、表单、弹窗等常用 UI 组件</span></span>
-<span class="line"><span>---</span></span></code></pre></div>`,1)])])}const h=n(t,[["render",l]]);export{_ as __pageData,h as default};
+<span class="line"><span>    details: 按钮、表单、弹窗等常用 UI 组件</span></span></code></pre></div>`,1)])])}const h=n(t,[["render",l]]);export{_ as __pageData,h as default};
